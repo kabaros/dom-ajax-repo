f8eb5067a108cdda53cfffef474cc53e5e64750d
Islam Dudaev <islamwolf23@gmail.com>
2018-04-03 21:12:15 +0100

    Add loader image

--- js/my-github.js
+++ js/my-github.js
@@ -1,9 +1,11 @@
 var reposCount = document.querySelector("#repos-count");
 var reposList = document.querySelector("#repos-list");
+var loader = document.querySelector(".loader");
 var count = 0;
 fetch("https://api.github.com/users/islam95/repos")
 	.then(response => response.json())
 	.then(json => {
+        loader.style.display = "none";
         json.forEach(function(repo){
             count++;
             reposCount.innerText = count;
