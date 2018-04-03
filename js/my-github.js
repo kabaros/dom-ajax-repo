b3eafce85947fb8baa9a15e0751ec3a63a8ce2f4
Islam Dudaev <islamwolf23@gmail.com>
2018-04-03 20:43:17 +0100

    Complete Part 1
    Show my list of repos with all links and a number of repos

--- js/my-github.js
+++ js/my-github.js
@@ -1,2 +1,14 @@
-// Write code here to communicate with Github
-
+var reposCount = document.querySelector("#repos-count");
+var reposList = document.querySelector("#repos-list");
+var count = 0;
+fetch("https://api.github.com/users/islam95/repos")
+	.then(response => response.json())
+	.then(json => {
+        json.forEach(function(repo){
+            count++;
+            reposCount.innerText = count;
+            var li = document.createElement("li");
+            li.innerHTML = `<a href="${repo.html_url}">` + repo.name + '</a>';
+            reposList.appendChild(li);
+        });
+	});
