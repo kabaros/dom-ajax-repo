32964528e2d9b495e9cf334d02d8fe9ef4f4a953
Islam Dudaev <islamwolf23@gmail.com>
2018-04-03 21:33:52 +0100

    Display all pull requests

--- js/my-github.js
+++ js/my-github.js
@@ -2,6 +2,7 @@ var reposCount = document.querySelector("#repos-count");
 var reposList = document.querySelector("#repos-list");
 var loader = document.querySelector(".loader");
 var count = 0;
+
 fetch("https://api.github.com/users/islam95/repos")
 	.then(response => response.json())
 	.then(json => {
@@ -10,7 +11,7 @@ fetch("https://api.github.com/users/islam95/repos")
             count++;
             reposCount.innerText = count;
             var li = document.createElement("li");
-            li.innerHTML = `<a href="${repo.html_url}">` + repo.name + '</a>';
+            li.innerHTML = `<a href="${repo.html_url}">${repo.name}</a>`;
             reposList.appendChild(li);
         });
 	});
---
+++ js/pull-requests.js
@@ -0,0 +1,13 @@
+var pullRequestList = document.querySelector("#pull-requests-list");
+var loader = document.querySelector(".loader");
+
+fetch("https://api.github.com/repos/codeyourfuture/js-exercises/pulls")
+	.then(response => response.json())
+	.then(json => {
+		loader.style.display = "none";
+		json.forEach(function(pullRequest) {
+			var li = document.createElement("li");
+			li.innerHTML = `<a href="${pullRequest.html_url}">${pullRequest.title}</a>`;
+			pullRequestList.appendChild(li);
+		});
+	});
