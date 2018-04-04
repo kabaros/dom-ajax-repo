33bcc1b4df75c2f686fa44b63e43ff499e17c8a2
Islam Dudaev <islamwolf23@gmail.com>
2018-04-04 02:59:43 +0100

    Check other users repos method

--- js/my-github.js
+++ js/my-github.js
@@ -1,17 +1,49 @@
 var reposCount = document.querySelector("#repos-count");
 var reposList = document.querySelector("#repos-list");
+var input = document.querySelector("#search-box");
+var searchBtn = document.querySelector("#searchBtn");
+
 var loader = document.querySelector(".loader");
 var count = 0;
 
+reposList.innerHTML = "";
 fetch("https://api.github.com/users/islam95/repos")
 	.then(response => response.json())
 	.then(json => {
-        loader.style.display = "none";
-        json.forEach(function(repo){
-            count++;
-            reposCount.innerText = count;
-            var li = document.createElement("li");
-            li.innerHTML = `<a href="${repo.html_url}">${repo.name}</a>`;
-            reposList.appendChild(li);
-        });
+		loader.style.display = "none";
+		json.forEach(repo => {
+			count++;
+			reposCount.innerText = count;
+			var li = document.createElement("li");
+			li.innerHTML = `<a href="${repo.html_url}">${repo.name}</a>`;
+			reposList.appendChild(li);
+		});
 	});
+
+searchBtn.addEventListener("click", function() {
+	if (input.value !== "") {
+		reposList.innerHTML = "";
+		fetch(`https://api.github.com/users/${input.value}/repos`)
+			.then(response => response.json())
+			.then(json => {
+				loader.style.display = "none";
+				json.forEach(repo => {
+					var li = document.createElement("li");
+					li.innerHTML = `<a href="${repo.html_url}">${repo.name}</a>`;
+					reposList.appendChild(li);
+				});
+			});
+	} else {
+		reposList.innerHTML = "";
+		fetch("https://api.github.com/users/islam95/repos")
+			.then(response => response.json())
+			.then(json => {
+				loader.style.display = "none";
+				json.forEach(repo => {
+					var li = document.createElement("li");
+					li.innerHTML = `<a href="${repo.html_url}">${repo.name}</a>`;
+					reposList.appendChild(li);
+				});
+			});
+	}
+});
