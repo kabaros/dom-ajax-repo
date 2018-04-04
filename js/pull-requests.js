859362518ac24489ba8faf8581f08501f61c84a7
Islam Dudaev <islamwolf23@gmail.com>
2018-04-04 01:50:20 +0100

    Add search box to filter pull requests by user

--- js/pull-requests.js
+++ js/pull-requests.js
@@ -1,14 +1,25 @@
 var pullRequestList = document.querySelector("#pull-requests-list");
 var loader = document.querySelector(".loader");
+loader.style.display = "none";
+var input = document.querySelector("#search-box");
 
-fetch("https://api.github.com/repos/codeyourfuture/js-exercises/pulls")
-	.then(response => response.json())
-	.then(json => {
-		loader.style.display = "none";
-        json.filter(element => element.user.login === 'islam95')
-        .forEach(pullRequest => {
-			var li = document.createElement("li");
-			li.innerHTML = `<a href="${pullRequest.html_url}">${pullRequest.title}</a>`;
-			pullRequestList.appendChild(li);
-		});
-	});
+input.addEventListener("keyup", function(event) {
+	const value = event.target.value;
+	//console.log(value);
+	if (value !== "") {
+		fetch("https://api.github.com/repos/codeyourfuture/js-exercises/pulls")
+			.then(response => response.json())
+			.then(json => {
+				json
+					.filter(element => element.user.login === value)
+					.forEach(pullRequest => {
+						var li = document.createElement("li");
+						li.innerHTML = `<a href="${pullRequest.html_url}">${
+							pullRequest.title
+						}</a>`;
+						pullRequestList.appendChild(li);
+					});
+			});
+	}
+    pullRequestList.innerHTML = "";
+});
