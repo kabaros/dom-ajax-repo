fc46f23e352862a9a267988000aad2e77f181e4f
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-04-06 15:33:37 +0100

    requirements1,2,3

--- js/my-github.js
+++ js/my-github.js
@@ -1,4 +1,19 @@
 // Write code here to communicate with Github
+function appendRepoLink(repo) {
+  //console.log(repo.name);
+  var container = document.querySelector("#repos-list");
+  var repoLi = document.createElement("li");
+  var repoAnchor = document.createElement("a");
+  repoAnchor.setAttribute("href", repo.html_url);
+  repoAnchor.innerText = repo.name;
+  repoLi.appendChild(repoAnchor);
+  container.appendChild(repoLi);
+}
+
+function displayRepoCount(count) {
+  var repoCountAnchor = document.querySelector("#repos-count");
+  repoCountAnchor.innerText = count;
+}
 
 var myGitHubLink = document.getElementById("repos-list");
 fetch("https://api.github.com/users/okolochuks/repos")
@@ -6,11 +21,6 @@ fetch("https://api.github.com/users/okolochuks/repos")
     return response.json();
   })
   .then(function(messages) {
-    messages.forEach(function(message) {
-      console.log(message.name);
-
-      var newRepo = document.createElement("li");
-      newRepo.innerText = message.name;
-      myGitHubLink.appendChild(newRepo);
-    });
+    messages.forEach(appendRepoLink);
+    displayRepoCount(messages.length);
   });
