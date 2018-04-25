c622524cd25474400c2006a091c833f5eeac9859
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-04-25 15:42:02 +0100

    show-all-pull-requests

---
+++ js/pull-requests.js
@@ -0,0 +1,20 @@
+function appendPullRequestLink(pullRequest) {
+  console.log(pullRequest.title);
+  var container = document.querySelector("#pull-requests-list");
+  var repoLi = document.createElement("li");
+  var repoAnchor = document.createElement("a");
+  repoAnchor.setAttribute("href", pullRequest.html_url);
+  repoAnchor.innerText = pullRequest.title;
+  repoLi.appendChild(repoAnchor);
+  container.appendChild(repoLi);
+}
+
+// var myPullRequestLink = document.getElementById("pull-Request-list");
+fetch("https://api.github.com/repos/codeyourfuture/js-exercises/pulls")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(messages) {
+    messages.forEach(appendPullRequestLink);
+    displayPullRequestCount(messages.length);
+  });
